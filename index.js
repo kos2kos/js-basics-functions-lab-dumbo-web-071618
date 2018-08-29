e86b267257cd362ca55ba8b602289a417c1319bf
Nkosi Enciso Givhan <koasterman@gmail.com>
2018-08-29 19:58:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+function distanceTravelledInFeet(arg1, arg2){
+  return Math.abs((arg1 - arg2) * 264);
+}
+
+
+function distanceFromHqInBlocks(arg1){
+  return Math.abs(arg1 - 42);
+}
+
+
+function distanceFromHqInFeet (arg1){
+  return distanceFromHqInBlocks(arg1) * 264;
+}
+
+
+
+function calculatesFarePrice(start, destination){
+  return 0;
+}
